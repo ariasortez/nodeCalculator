9efea5bb5abcdf488a9a50819747509e92a0acd1
ariasortez <juan.arias@unitec.edu>
2023-12-26 22:01:33 -0600

    We have implemented an example of clean architecture using Node.js

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,14 +1,25 @@
 import { CreateTable } from '../domain/use-cases/create-table.use-case';
+import { SaveFile } from '../domain/use-cases/save-file.use-case';
 
 interface RunOptions {
   base: number;
   limit: number;
   showTable: boolean;
+  name: string;
+  destination: string;
 }
 
 export class ServerApp {
-  static run({ base, limit, showTable }: RunOptions) {
+  static run({ base, limit, showTable, name, destination }: RunOptions) {
     const table = new CreateTable().execute({ base, limit });
+    const fileCreated = new SaveFile().execute({
+      fileContent: table,
+      fileName: name,
+      fileDestination: destination,
+    });
     if (showTable) console.log(table);
+    fileCreated
+      ? console.log('File created :)')
+      : console.error('Unable to create file');
   }
 }
