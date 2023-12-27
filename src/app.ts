14d475d0d5acf9846402404362016245c315c187
ariasortez <juan.arias@unitec.edu>
2023-12-26 20:37:21 -0600

    Clean Architecture/domain

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { yarg } from './config/plugins/yargs.plugin';
+import { ServerApp } from './presentation/server-app';
 
 (async () => {
   await main();
@@ -6,5 +7,6 @@ import { yarg } from './config/plugins/yargs.plugin';
 })();
 
 async function main() {
-  console.log(yarg);
+  const { b: base, l: limit, s: showTable } = yarg;
+  ServerApp.run({ base, limit, showTable });
 }
